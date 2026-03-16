5f5dfeb89abe2bfb27fa3e1c52c6d72fad921507
makerjun <72922397+imakerjun@users.noreply.github.com>
2026-03-16 11:21:29 +0900

    auto: 파일 변경사항 자동 저장 (2026-03-16 11:21:29)

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,1 +1,22 @@
-export { useMDXComponents } from 'nextra-theme-docs'
+import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs'
+import { Mermaid } from './app/components/Mermaid'
+
+const docsComponents = getDocsMDXComponents()
+
+export function useMDXComponents(components) {
+  return {
+    ...docsComponents,
+    ...components,
+    pre: ({ children, ...props }) => {
+      const child = children as any
+      if (
+        child?.props?.className === 'language-mermaid' ||
+        child?.props?.['data-language'] === 'mermaid'
+      ) {
+        return <Mermaid chart={child.props.children as string} />
+      }
+      const Pre = docsComponents.pre ?? 'pre'
+      return <Pre {...props}>{children}</Pre>
+    },
+  }
+}
